272fe685b381463a7743ce8ff701eccdc9f3e733
sungmin-choi <namja306@naver.com>
2022-04-21 10:43:51 +0900

    style) font-family : 'Fira Mono', monospace 으로 수정

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  .left-container {
+    margin-left: 1rem;
+    .logo {
+      font-size: 21px;
+      font-size: 700;
+    }
+  }
+  .right-container {
+    margin-right: 1rem;
+  }
+`;
 
 function Header() {
   return (
-    <header>
-      <div className="logo">Coala</div>
+    <HeaderWrapper>
+      <div className="left-container">
+        <div className="logo">Coala</div>
+      </div>
       <div className="right-container">
         <input type="search" />
         <button type="button" className="login-button">
           로그인
         </button>
       </div>
-    </header>
+    </HeaderWrapper>
   );
 }
 
