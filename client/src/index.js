d20a7b6ac7822388c27dc30bedf36f36365bbe9a
민혁 <dygks0602@naver.com>
2022-04-21 09:14:55 +0900

    merge1-1

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import './index.css';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
-=======
-import App from './App';
->>>>>>> 08671576f4325033c6c4457dc3765ddb72196a11
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,10 +8,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-<<<<<<< HEAD
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// reportWebVitals();
-=======
->>>>>>> 08671576f4325033c6c4457dc3765ddb72196a11
