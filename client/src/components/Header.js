7af2c6a0bcf19a8aa6c3e208e8e546f49409edee
sungmin-choi <namja306@naver.com>
2022-04-21 10:29:12 +0900

    add)styled-components

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="logo">Coala</div>
+      <div className="right-container">
+        <input type="search" />
+        <button type="button" className="login-button">
+          로그인
+        </button>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
