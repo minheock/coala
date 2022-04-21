29744a50df95ef9f17eeaa70aae547b90b14db5a
duwls8910 <duwls8910@gmail.com>
2022-04-21 18:32:02 +0900

    add controllers/users

--- server/models/like.js
+++ server/models/like.js
@@ -9,6 +9,14 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.belongsTo(models.contents, {
+        foreignKey: 'content_id',
+        sourceKey: 'id',
+      });
+      this.belongsTo(models.users, {
+        foreignKey: 'user_id',
+        sourceKey: 'id',
+      });
     }
   }
   like.init(
--- server/models/users.js
+++ server/models/users.js
@@ -9,6 +9,14 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.hasMany(models.contents, {
+        foreignKey: 'user_id',
+        sourceKey: 'id',
+      });
+      this.hasMany(models.like, {
+        foreignKey: 'user_id',
+        sourceKey: 'id',
+      });
     }
   }
   users.init(
