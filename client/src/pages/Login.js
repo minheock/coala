9eddc42b0a10ab8947b6aa08d680d2ecad189d5e
민혁 <dygks0602@naver.com>
2022-04-22 10:41:03 +0900

    패키지 eslint 수정

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,1 +1,15 @@
-// import react from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const LoginWrapper = styled.div``;
+// 123
+function Login() {
+  const [isLogin, setisLogin] = useState(false);
+  return (
+    <LoginWrapper>
+      <div />
+    </LoginWrapper>
+  );
+}
+
+export default Login;
