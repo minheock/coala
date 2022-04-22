024f046365bb90b075b8661a67b50c37eb798e8e
sungmin-choi <namja306@naver.com>
2022-04-22 10:37:18 +0900

    config.js color 규칙 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
 import 'antd/dist/antd.min.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <NavBar />
       <h1>Coala</h1>
     </div>
   );
--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,9 @@
+// 반응형 상수
 export const XLView = 1437;
 export const LView = 1414;
 export const MView = 1040;
 export const SView = 752;
+
+// color
+export const CoalaGrey = '#dfe6e9';
+export const CoalaGreen = '#00b894';
