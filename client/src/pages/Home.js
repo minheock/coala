a4b07dbf5d2d2605fcc847bd2711b1af47a1809c
sungmin-choi <namja306@naver.com>
2022-04-24 01:27:26 +0900

    feat) 기본 fibase 설정및 이미지업로드 기능 구현" " " ""
    "
    "
    "
    "

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Header from '../components/Header';
 import NavBar from '../components/NavBar';
+import Contents from '../components/Contents';
+import uploadFiles from '../firebase';
 
 function Home() {
+  const formHandler = e => {
+    e.preventDefault();
+    const file = e.target[0].files[0];
+    uploadFiles(file);
+  };
   return (
     <div>
       <Header page="Home" />
       <NavBar />
-      <h1>Home</h1>
+      <Contents />
+      <form onSubmit={formHandler}>
+        <input type="file" className="input" />
+        <button type="submit">Upload</button>
+      </form>
     </div>
   );
 }
