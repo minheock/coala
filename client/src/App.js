768dbc55259d6b42edaaa1a5adef318d25987c0c
민혁 <dygks0602@naver.com>
2022-04-24 20:13:18 +0900

    merge2.2

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import 'antd/dist/antd.min.css';
-import NavBar from './components/NavBar';
-import Header from './components/Header';
+// import NavBar from './components/NavBar';
+// import Header from './components/Header';
 import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
 import Home from './pages/Home';
@@ -11,19 +11,17 @@ import ContentDetail from './pages/ContentDetail';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/write" element={<Post />} />
-          <Route path="/content/detail" element={<ContentDetail />} />
-        </Routes>
-        <NavBar />
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      {/* <Header /> */}
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/write" element={<Post />} />
+        <Route path="/content/detail" element={<ContentDetail />} />
+      </Routes>
+      {/* <NavBar /> */}
+    </div>
   );
 }
 
