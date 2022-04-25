d36615423e0f3a724486206cb05b47a7c439473f
민혁 <dygks0602@naver.com>
2022-04-25 22:18:03 +0900

    signup, assept styled component 수정

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -43,69 +43,73 @@ function Signup() {
         <SignupDiv>
           <SignLogo to="/">Coala</SignLogo>
           <form onSubmit={handleSubmit}>
-            <InputNameBox>
-              <InputTitle>이름</InputTitle>
-              <InputName
+            <div className="InputBox">
+              <div className="InputTitle">이름</div>
+              <input
+                className="InputName"
                 type="text"
                 name="inputName"
                 placeholder="이름을 입력하세요."
                 onChange={e => handleInputValue('userName', e)}
               />
-            </InputNameBox>
-            <InputEmailBox>
-              <InputTitle>이메일</InputTitle>
-              <InputEmail
+            </div>
+            <div className="InputBox">
+              <div className="InputTitle">이메일</div>
+              <input
+                className="InputEmail"
                 type="text"
                 name="inputEmail"
                 placeholder="이메일을 입력하세요."
                 onChange={e => handleInputValue('email', e)}
               />
-            </InputEmailBox>
-            <InputPasswordBox>
-              <InputTitle>비밀번호</InputTitle>
-              <InputPassword
+            </div>
+            <div className="InputBox">
+              <div className="InputTitle">비밀번호</div>
+              <input
+                className="InputPassword"
                 type="password"
                 name="inputPassword"
                 placeholder="비밀번호를 입력하세요."
                 onChange={e => handleInputValue('password', e)}
               />
-            </InputPasswordBox>
-            <SuccesPasswordBox>
-              <InputTitle>비밀번호 확인</InputTitle>
-              <InputPassword
+            </div>
+            <div className="InputBox">
+              <div className="InputTitle">비밀번호 확인</div>
+              <input
+                className="InputPasswordChecked"
                 type="password"
                 name="inputPassword"
                 placeholder="비밀번호를 입력하세요."
                 onChange={e => handleInputValue('passwordChecked', e)}
               />
-            </SuccesPasswordBox>
-            <SignupBtn>회원가입</SignupBtn>
+            </div>
+            <button type="button" className="SignupBtn">
+              회원가입
+            </button>
           </form>
         </SignupDiv>
       ) : (
         <AcceptDiv>
-          <Link classname="home-link" to="/">
-            Coala
-          </Link>
-          <TermsBox>
-            <AcceptTitle>코알라 이용약관에 모두 동의합니다</AcceptTitle>
-          </TermsBox>
-          <TermsBox>
-            <AcceptTitle>코알라 이용약관 동의</AcceptTitle>
-            <AcceptBox type="text" />
-          </TermsBox>
-          <TermsBox>
-            <AcceptTitle>개인정보 수집 및 이용동의</AcceptTitle>
-            <AcceptBox type="text" />
-          </TermsBox>
-          <TermsBox>
-            <AcceptTitle>위치정보 이용약관 동의</AcceptTitle>
-            <AcceptBox type="text" />
-          </TermsBox>
-          <TermsBox>
-            <AcceptTitle>프로모션 정보 수신 동의</AcceptTitle>
-            <AcceptBox type="text" />
-          </TermsBox>
+          <SignLogo to="/">Coala</SignLogo>
+          <div className="termsBox">
+            <div className="acceptTitle">코알라 이용약관에 모두 동의합니다</div>
+          </div>
+          <div className="termsBox">
+            <div className="acceptTitle">코알라 이용약관 동의</div>
+            <div className="AcceptBox" type="text" />
+          </div>
+          <div className="termsBox">
+            <div className="acceptTitle">개인정보 수집 및 이용동의</div>
+            <div className="AcceptBox" type="text" />
+          </div>
+          <div className="termsBox">
+            <div className="acceptTitle">위치정보 이용약관 동의</div>
+            <div className="AcceptBox" type="text" />
+          </div>
+          <div className="termsBox">
+            <div className="acceptTitle">프로모션 정보 수신 동의</div>
+            <div className="AcceptBox" type="text" />
+          </div>
           <button type="button" className="accept-btn" onClick={handleAccept}>
             확인
           </button>
@@ -114,82 +118,7 @@ function Signup() {
     </>
   );
 }
-
-/* */
-const SignupDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  /* margin-top: 80px; */
-  /* height: 100%; */
-  /* margin-bottom: 80px; */
-`;
-
-const InputNameBox = styled.div`
-  margin-top: 40px;
-`;
-const InputPasswordBox = styled.div`
-  margin-top: 40px;
-`;
-const SuccesPasswordBox = styled.div`
-  margin-top: 40px;
-`;
-
-const InputEmailBox = styled.div`
-  margin-top: 40px;
-`;
-
-const InputTitle = styled.div`
-  text-align: left;
-  font-size: 15px;
-  font-weight: bold;
-`;
-
-const InputName = styled.input`
-  margin-top: 5px;
-  width: 350px;
-  height: 45px;
-  border-color: #999999;
-  border-top: none;
-  border-left: none;
-  border-right: none;
-  border-width: 1px;
-`;
-const InputPassword = styled.input`
-  margin-top: 5px;
-  width: 350px;
-  height: 45px;
-  border-color: #999999;
-  border-top: none;
-  border-left: none;
-  border-right: none;
-  border-width: 1px;
-`;
-
-const InputEmail = styled.input`
-  margin-top: 5px;
-  width: 350px;
-  height: 45px;
-  border-color: #999999;
-  border-top: none;
-  border-left: none;
-  border-right: none;
-  border-width: 1px;
-`;
-
-const SignupBtn = styled.button`
-  margin-top: 45px;
-  border-radius: 10px;
-  border: none;
-  width: 350px;
-  height: 55px;
-  font-size: 20px;
-  font-weight: bold;
-  background-color: #555555;
-  color: white;
-  cursor: pointer;
-`;
+// 사이트 로고
 const SignLogo = styled(Link)`
   color: black;
   text-decoration-line: none;
@@ -200,33 +129,84 @@ const SignLogo = styled(Link)`
   text-align: center;
   cursor: pointer;
   transition: 1s;
-  /* &:hover {
-    color: #555555;
-  } */
 `;
-// Accept
+// 회원가입 화면
+const SignupDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .InputBox {
+    margin-top: 40px;
+  }
+  .InputTitle {
+    text-align: left;
+    font-size: 15px;
+    font-weight: bold;
+  }
+  .SignupBtn {
+    margin-top: 45px;
+    border-radius: 10px;
+    border: none;
+    width: 350px;
+    height: 55px;
+    font-size: 20px;
+    font-weight: bold;
+    background-color: #555555;
+    color: white;
+    cursor: pointer;
+  }
+  .InputName {
+    margin-top: 5px;
+    width: 350px;
+    height: 45px;
+    border-color: #999999;
+    border-top: none;
+    border-left: none;
+    border-right: none;
+    border-width: 1px;
+  }
+  .InputPassword {
+    margin-top: 5px;
+    width: 350px;
+    height: 45px;
+    border-color: #999999;
+    border-top: none;
+    border-left: none;
+    border-right: none;
+    border-width: 1px;
+  }
+  .InputPasswordChecked {
+    margin-top: 5px;
+    width: 350px;
+    height: 45px;
+    border-color: #999999;
+    border-top: none;
+    border-left: none;
+    border-right: none;
+    border-width: 1px;
+  }
+  .InputEmail {
+    margin-top: 5px;
+    width: 350px;
+    height: 45px;
+    border-color: #999999;
+    border-top: none;
+    border-left: none;
+    border-right: none;
+    border-width: 1px;
+  }
+`;
+
+// 약관동의 화면
 const AcceptDiv = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin-top: 80px;
   margin-bottom: 80px;
-  .home-link {
-    color: black;
-    text-decoration-line: none;
-    font-size: 50px;
-    font-weight: 600;
-    font-style: italic;
-    text-align: center;
-    cursor: pointer;
-    transition: 2s;
-    &:hover {
-      color: #555555;
-    }
-  }
   .accept-btn {
-    padding-top: 10px;
+    padding-top: 5px;
     text-align: center;
     margin-top: 45px;
     border-radius: 10px;
@@ -239,26 +219,23 @@ const AcceptDiv = styled.div`
     color: white;
     cursor: pointer;
   }
-`;
-
-const TermsBox = styled.div`
-  margin-top: 20px;
-`;
-
-const AcceptTitle = styled.div`
-  margin-bottom: 10px;
-  text-align: left;
-  font-size: 17px;
-  font-weight: bold;
-`;
-
-const AcceptBox = styled.div`
-  overflow: auto;
-  margin-top: 1px;
-  width: 350px;
-  height: 100px;
-  border: 1px solid grey;
-  border-width: 1px;
+  .termsBox {
+    margin-top: 20px;
+  }
+  .AcceptBox {
+    overflow: auto;
+    margin-top: 1px;
+    width: 350px;
+    height: 100px;
+    border: 1px solid grey;
+    border-width: 1px;
+  }
+  .acceptTitle {
+    margin-bottom: 10px;
+    text-align: left;
+    font-size: 17px;
+    font-weight: bold;
+  }
 `;
 
 export default Signup;
